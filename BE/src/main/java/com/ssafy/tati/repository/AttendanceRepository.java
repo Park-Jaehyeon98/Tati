26f725232d3cca32fea88ebc762a5226ccae2883
sylee723 <seoyeonlee0723@gmail.com>
2023-08-07 17:29:50 +0900

    fix : 캠스터디 퇴실 요청 없을 때 처리

--- BE/src/main/java/com/ssafy/tati/repository/AttendanceRepository.java
+++ BE/src/main/java/com/ssafy/tati/repository/AttendanceRepository.java
@@ -1,7 +1,15 @@
 package com.ssafy.tati.repository;
 
 import com.ssafy.tati.entity.Attendance;
+import com.ssafy.tati.entity.StudyMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface AttendanceRepository extends JpaRepository<Attendance, Integer> {
+    Optional<Attendance> findByStudyMemberAndInTimeBetween(StudyMember studyMember, LocalDateTime startOfDay, LocalDateTime endOfDay);
+
+    List<Attendance> findAllByIsAttendedIsNull();
 }
