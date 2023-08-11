fd10dff247b549d457c1f5495352a22fbe725d00
zhddkfxks1@naver.com <zhddkfxks1@naver.com>
2023-08-11 09:40:32 +0900

    fix : 공지사항 프론트 요청 수정

--- fe/src/api/apiClient.js
+++ fe/src/api/apiClient.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "https://i9b305.p.ssafy.io/api",
+  // baseURL: "https://i9b305.p.ssafy.io/api",
+  baseURL: "https://localhost:8080",
   // baseURL: "http://192.168.31.57:8080",
 });
