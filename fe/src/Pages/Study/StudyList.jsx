b1699ec2a3c47c9cd5553887744d45bd64d15458
김효인 <bright_bat@naver.com>
2023-08-14 10:24:03 +0900

    fix: 스터디리스트 페이지네이션 수정

--- fe/src/Pages/Study/StudyList.jsx
+++ fe/src/Pages/Study/StudyList.jsx
@@ -4,17 +4,19 @@ import { apiClient } from "../../api/apiClient";
 import style from './StudyList.module.css';
 
 import StudyCardItem from "../../Components/Common/StudyCardItem";
+import { Button } from '@material-ui/core';
 
 
 const StudyList = () => {
     const navigate = useNavigate();
 
     // 스터디 리스트
-    const [studyList, setStudyList] = useState([1, 2]);
+    const [studyList, setStudyList] = useState([]);
     // 페이지네이션
-    const [currentPage, setCurrentPage] = useState(1);
+    const [currentPage, setCurrentPage] = useState(0);
     const [totalPages, setTotalPages] = useState(1);
-    const firstPage = currentPage - (currentPage % 5) + 1
+    const zeroToOnePage = currentPage + 1
+    const firstPage = zeroToOnePage - (zeroToOnePage % 5) + 1
     // 카테고리 선택
     const [categoryId, setCategoryId] = useState(1);
     const categoryIdArray = ["전체", "자격증", "취업", "학교", "공시", "기타"];
@@ -30,7 +32,7 @@ const StudyList = () => {
             .then((res) => {
                 console.log(res)
                 setStudyList(res.data.content)
-                setTotalPages(res.data.sort.totalPages)
+                setTotalPages(res.data.totalPages)
             })
             .catch((err) => {
                 console.log(err)
@@ -68,7 +70,8 @@ const StudyList = () => {
             }
         })
             .then((res) => {
-                console.log(res)
+                setStudyList(res.data.content)
+                setTotalPages(res.data.totalPages)
             })
             .catch((err) => {
                 console.log(err)
@@ -103,7 +106,7 @@ const StudyList = () => {
             <>
                 <h3>스터디 목록</h3>
                 <hr />
-                <button onClick={() => { navigate('Create') }}>스터디 만들기</button>
+                <Button onClick={() => { navigate('Create') }}>스터디 만들기</Button>
                 <div>
                     {categoryIdArray.map((categoryIdItem, index) =>
                         <button key={categoryIdItem} className={index === categoryId - 1 ? style.selected : style.noSelected} onClick={() => handleCategoryIdClick(index)}>{categoryIdItem}</button>
@@ -111,16 +114,16 @@ const StudyList = () => {
                 </div>
                 <div>
                     <input type="text" name="keywordInput" value={keywordInput} onChange={handleKeywordInputChagne} placeholder="스터디이름으로 검색" />
-                    <button onClick={handleSearchBtnClick}>검색</button>
+                    <Button onClick={handleSearchBtnClick}>검색</Button>
                 </div>
 
-                <button onClick={() => {
+                <Button onClick={() => {
                     console.log({
                         page: currentPage,
                         category: categoryId,
                         keyword: keyword
                     })
-                }}>인풋값 콘솔 출력 테스트용</button>
+                }}>인풋값 콘솔 출력 테스트용</Button>
             </>
 
             <hr />
@@ -136,40 +139,40 @@ const StudyList = () => {
 
 
                 {/* 페이지네이션 부분 */}
-                <button
+                <Button
                     key={'<'}
                     onClick={() => setCurrentPage(currentPage - 1)}
-                    disabled={currentPage === 1}
+                    disabled={currentPage === 0}
                 >
                     {"<"}
-                </button>
+                </Button>
                 {Array.from({ length: 5 }, (_, i) =>
-                    currentPage % 5 === 0 ?
+                    zeroToOnePage % 5 === 0 ?
                         (firstPage - 5 + i) <= totalPages &&
-                        <button
+                        <Button
                             key={firstPage - 5 + i}
-                            onClick={() => setCurrentPage(firstPage - 5 + i)}
-                            disabled={firstPage - 5 + i === currentPage}
+                            onClick={() => setCurrentPage(firstPage - 5 + i - 1)}
+                            disabled={firstPage - 5 + i === currentPage + 1}
                         >
                             {firstPage - 5 + i}
-                        </button>
+                        </Button>
                         :
-                        (firstPage + i) <= totalPages && <button
+                        (firstPage + i) <= totalPages && <Button
                             key={firstPage + i}
-                            onClick={() => setCurrentPage(firstPage + i)}
-                            disabled={firstPage + i === currentPage}
+                            onClick={() => setCurrentPage(firstPage + i - 1)}
+                            disabled={firstPage + i === currentPage + 1}
                         >
                             {firstPage + i}
-                        </button>)}
-                <button
+                        </Button>)}
+                <Button
                     key={'>'}
                     onClick={() => {
                         setCurrentPage(currentPage + 1)
                     }}
-                    disabled={currentPage === totalPages || !totalPages}
+                    disabled={currentPage === totalPages - 1 || !totalPages}
                 >
                     {">"}
-                </button>
+                </Button>
             </div>
         </div>
     )
