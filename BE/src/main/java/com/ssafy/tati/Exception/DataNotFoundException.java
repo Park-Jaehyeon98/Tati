d2f9cc968fa6681d25d8e189d27947130814e928
박영기 <rl0428@naver.com>
2023-08-18 10:59:32 +0900

    fix: 디렉토리 수정

--- BE/src/main/java/com/ssafy/tati/Exception/DataNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.tati.Exception;
-
-public class DataNotFoundException extends RuntimeException{
-    public DataNotFoundException(String message){
-        super(message);
-    }
-}
