b882c6c510b0c902dfa8296685cf01a246e6728b
박재현 <10225-holizon9@users.noreply.lab.ssafy.com>
2023-08-18 09:24:21 +0900

    fix : merge 오류 해결

--- BE/src/main/java/com/ssafy/tati/Exception/MismatchDataException.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.tati.Exception;
-
-public class MismatchDataException extends RuntimeException {
-    public MismatchDataException(String message){
-        super(message);
-    }
-}
